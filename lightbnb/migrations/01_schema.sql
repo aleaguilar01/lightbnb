9accc4cf9e6d7231acf310476158966f585b1192
Alejandra Aguilar <aleaguilar01@gmail.com>
2024-05-10 01:03:59 -0600

    Add NOT NULL and ON DELETE CASCADE statements.

---
+++ lightbnb/migrations/01_schema.sql
@@ -0,0 +1,72 @@
+DROP TABLE IF EXISTS users CASCADE;
+
+DROP TABLE IF EXISTS reservations CASCADE;
+
+DROP TABLE IF EXISTS property_reviews CASCADE;
+
+DROP TABLE IF EXISTS properties CASCADE;
+
+DROP TABLE IF EXISTS countries CASCADE;
+
+DROP TABLE IF EXISTS provinces CASCADE;
+
+DROP TABLE IF EXISTS cities CASCADE;
+
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE countries (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE provinces (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  country_id INTEGER REFERENCES countries(id) ON DELETE CASCADE
+);
+
+CREATE TABLE cities (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  province_id INTEGER REFERENCES provinces(id) ON DELETE CASCADE
+);
+
+CREATE TABLE properties (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  city_id INTEGER REFERENCES cities(id) ON DELETE CASCADE,
+  title VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  cost_per_night INTEGER NOT NULL,
+  parking_spaces INTEGER NOT NULL,
+  number_of_bathrooms INTEGER NOT NULL,
+  number_of_bedrooms INTEGER NOT NULL,
+  url_coverphoto VARCHAR(255) NOT NULL,
+  url_thumbnail VARCHAR(255) NOT NULL,
+  street VARCHAR(255) NOT NULL,
+  post_code VARCHAR(255) NOT NULL,
+  active BOOLEAN NOT NULL
+);
+
+CREATE TABLE reservations (
+  id SERIAL PRIMARY KEY NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE property_reviews (
+  id SERIAL PRIMARY KEY NOT NULL,
+  message TEXT NOT NULL,
+  rating SMALLINT NOT NULL,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE
+);
